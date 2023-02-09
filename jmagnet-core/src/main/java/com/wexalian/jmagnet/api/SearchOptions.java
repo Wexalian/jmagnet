688977f77a67fe4060be40072ad3869171d619c6
Wexalian <larsgerrits+wexalian@github.com>
2023-02-09 05:48:06 +0100

    Cleanup `SearchOptions`

--- jmagnet-core/src/main/java/com/wexalian/jmagnet/api/SearchOptions.java
+++ jmagnet-core/src/main/java/com/wexalian/jmagnet/api/SearchOptions.java
@@ -1,24 +1,28 @@
 package com.wexalian.jmagnet.api;
 
-public record SearchOptions(String imdbId, String slug, int season, int episode, int limit, int pages) {
+public record SearchOptions(String imdbId, String slug, int seasonNum, int episodeNum, int limit, int pages) {
     public SearchOptions() {
         this("", "", -1, -1, -1, 1);
         throw new IllegalStateException("Please use one of the factory methods to ensure proper settings");
     }
     
-    public static SearchOptions imdb(String imdbID) {
+    public static SearchOptions searchImdb(String imdbID) {
         return new SearchOptions(imdbID, "", -1, -1, -1, 1);
     }
     
-    public static SearchOptions show(String slug) {
-        return new SearchOptions("", slug, -1, -1, -1, 1);
+    public static SearchOptions searchSlug(String imdbID, String slug) {
+        return new SearchOptions(imdbID, slug, -1, -1, -1, 1);
     }
     
-    public static SearchOptions season(String slug, int season) {
-        return new SearchOptions("", slug, season, -1, -1, 1);
+    public static SearchOptions searchSeason(String imdbID, String slug, int season) {
+        return new SearchOptions(imdbID, slug, season, -1, -1, 1);
     }
     
-    public static SearchOptions episode(String slug, int season, int episode) {
-        return new SearchOptions("", slug, season, episode, -1, 1);
+    public static SearchOptions searchEpisode(String imdbID, String slug, int season, int episode) {
+        return new SearchOptions(imdbID, slug, season, episode, -1, 1);
+    }
+    
+    public static SearchOptions searchPaged(String imdbID, String slug, int season, int episode, int limit, int pages) {
+        return new SearchOptions(imdbID, slug, season, episode, limit, pages);
     }
 }
