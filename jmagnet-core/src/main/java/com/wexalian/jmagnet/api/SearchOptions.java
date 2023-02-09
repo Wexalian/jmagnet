21934781cfc489c85cf544abdb456b6d715dfb82
Wexalian <larsgerrits+wexalian@github.com>
2023-02-09 05:14:07 +0100

    Make `SearchOptions#pages` 1 by default

--- jmagnet-core/src/main/java/com/wexalian/jmagnet/api/SearchOptions.java
+++ jmagnet-core/src/main/java/com/wexalian/jmagnet/api/SearchOptions.java
@@ -2,23 +2,23 @@ package com.wexalian.jmagnet.api;
 
 public record SearchOptions(String imdbId, String slug, int season, int episode, int limit, int pages) {
     public SearchOptions() {
-        this("", "", -1, -1, -1, -1);
+        this("", "", -1, -1, -1, 1);
         throw new IllegalStateException("Please use one of the factory methods to ensure proper settings");
     }
     
     public static SearchOptions imdb(String imdbID) {
-        return new SearchOptions(imdbID, "", -1, -1, -1, -1);
+        return new SearchOptions(imdbID, "", -1, -1, -1, 1);
     }
     
     public static SearchOptions show(String slug) {
-        return new SearchOptions("", slug, -1, -1, -1, -1);
+        return new SearchOptions("", slug, -1, -1, -1, 1);
     }
     
     public static SearchOptions season(String slug, int season) {
-        return new SearchOptions("", slug, season, -1, -1, -1);
+        return new SearchOptions("", slug, season, -1, -1, 1);
     }
     
     public static SearchOptions episode(String slug, int season, int episode) {
-        return new SearchOptions("", slug, season, episode, -1, -1);
+        return new SearchOptions("", slug, season, episode, -1, 1);
     }
 }
