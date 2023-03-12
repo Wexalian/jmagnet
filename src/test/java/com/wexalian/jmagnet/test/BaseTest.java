cf787cc5cfb48b9978cfde1557e785e3764a4287
Wexalian <larsgerrits+wexalian@github.com>
2023-03-12 01:33:11 +0100

    Remove old `BaseTest`

--- src/test/java/com/wexalian/jmagnet/test/BaseTest.java
+++
@@ -1,5 +0,0 @@
-package com.wexalian.jmagnet.test;
-
-public class BaseTest {
-    protected static final String MAGNET_URI_THE_FLASH_9X05 = "magnet:?xt=urn:btih:C965FB0AE42A4624D714BC1F81013C62D31CEB59&dn=The.Flash.2014.S09E05.HDTV.x264-TORRENTGALAXY&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce";
-}
